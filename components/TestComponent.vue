d7732aaa73433acc0ab0e1139d72a013e6f1733d
Serhii <havryliuk.serhii@icloud.com>
2022-07-07 11:52:47 +0300

    first commit

---
+++ components/TestComponent.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <p> Basic components </p>
+    <h1> Title h1</h1>
+    <h2> Title h2 </h2>
+    <h3> Title h3 </h3>
+    <h4> Title h4 </h4>
+    <h5> Title h5 </h5>
+    <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dignissim scelerisque nisi a imperdiet. Ut malesuada vel nunc id suscipit. Nullam dapibus orci a orci faucibus aliquet.</p>
+    <a href="#"> Link to</a>
+    <br>
+    <v-btn class="btn_primary" depressed color="primary">
+      Primary button
+    </v-btn>
+  </div>
+</template>
+
+<script>
+import { Vue, Component } from 'nuxt-property-decorator'
+import TestComp from '../components/TestComponent'
+
+export default @Component({
+  components: { TestComp }
+})
+
+class Index extends Vue {
+    title = 'Welcome to the ETS_F22_NV project!';
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <v-row justify="center" align="center">
+      <h1 class="headline">
+        {{ title }}
+      </h1>
+    </v-row>
+    <TestComp />
+  </div>
+</template>
+
+<script>
+import { Vue, Component } from 'nuxt-property-decorator'
+import TestComp from '../components/TestComponent'
+
+export default @Component({
+  components: { TestComp }
+})
+
+class Index extends Vue {
+  title = 'Welcome to the ETS_F22_NV project!';
+}
+</script>
